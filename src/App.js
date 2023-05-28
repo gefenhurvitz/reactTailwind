e7dfd2f94eaeeddb6721bb23ef7638c4ae54ea20
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-28 14:27:22 +0300

    done configure routes on the navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FooterCom from "./components/FooterCom";
 import NavBarCom from "./components/NavBarCom";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 // import { Routes, Route } from "react-router-dom";
 // import HomePage from "./pages/HomePage";
@@ -12,6 +14,9 @@ function App() {
       <NavBarCom />
       <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/contact" element={<ContactPage/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
       </Routes>
       <h1>hello</h1>
       <FooterCom />
