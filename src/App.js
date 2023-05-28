42b4a9526dcafe3d785ce57a47cb929955920350
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-28 13:09:40 +0300

    app working with navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import FooterCom from './components/FooterCom';
+import NavBarCom from './components/NavBarCom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-400">
+      <NavBarCom/>
+        <h1>hello</h1>
+        <FooterCom/>
     </div>
   );
 }
