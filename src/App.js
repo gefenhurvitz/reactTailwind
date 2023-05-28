3807bcffe0225d731dc175e65c0fc33d2d7f00f3
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-28 14:14:20 +0300

    start adding routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
-import './App.css';
-import FooterCom from './components/FooterCom';
-import NavBarCom from './components/NavBarCom';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import FooterCom from "./components/FooterCom";
+import NavBarCom from "./components/NavBarCom";
+import HomePage from "./pages/HomePage";
+// import { Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="bg-gray-400">
-      <NavBarCom/>
-        <h1>hello</h1>
-        <FooterCom/>
+      <NavBarCom />
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+      <h1>hello</h1>
+      <FooterCom />
     </div>
   );
 }
