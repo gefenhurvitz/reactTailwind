5c1028d1dd5e4aee4185169612610bf105f07058
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-29 12:20:58 +0300

    test new branch

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,6 +6,7 @@ const AboutPage = () => {
       <div>
         <h1>hello im gefen</h1>
         <h2>im a web developer</h2>
+        <hello> my name is gefen</hello>
       </div>
       
     </div>
